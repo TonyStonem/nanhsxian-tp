ed92858db2cb744326823f8f3c8a3be36d2cc0fd
xjwStudio <1521975316@qq.com>
2017-11-27 21:14:41 +0800

    新增Struts2

---
+++ src/cn/xjw/tpweb/action/HomeListAction.java
@@ -0,0 +1,14 @@
+package cn.xjw.tpweb.action;
+
+/**
+ * Created by xjw on 2017/11/27 21:03
+ * Email : 1521975316@qq.com
+ */
+public class HomeListAction {
+
+    public String execute() {
+        System.out.println("home list.");
+        return "success";
+    }
+
+}
